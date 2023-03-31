5cd65f9ab7ed0c5ae9d9f83ce165c2f2a051a110
ggoran_pvt <goran.gagro@gmail.com>
2023-03-31 17:08:02 +0200

    remove unused function

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -10,9 +10,9 @@ export function AppProvider(props) {
   const [config, setConfig] = useState(null);
   const [error, setError] = useState(null);
 
-  function handleSignOut() {
-    setUsername("");
-  }
+  // function handleSignOut() {
+  //   setUsername("");
+  // }
 
   useEffect(() => {
     fetch("/assets/config.json")
